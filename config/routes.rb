0984b879579ca54de861a2c9c31f3f51faf30b47
Joe Spampinato <jcspampinato@gmail.com>
2015-12-01 17:27:02 -0500

    added logout link, formatting home.index, repaired devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+ devise_for :users
   get 'sessions/index'
 
   get 'sessions/create'
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
-  devise_for :users
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
