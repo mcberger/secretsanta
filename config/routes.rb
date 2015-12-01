bf585954054b7097cfb206a07802dac002717a37
Joe Spampinato <jcspampinato@gmail.com>
2015-12-01 15:42:41 -0500

    fixes #2 created user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
+  get 'users/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
