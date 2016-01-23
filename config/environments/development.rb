2cafb6a4cd4be9a32c91ac19e3cdd3d6a18a5ae4
Mai Berger <m.c.berger@icloud.com>
2016-01-23 18:28:54 +0100

    fixed some mailer issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,15 +16,19 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.smtp_settings = {
-  domain: 'localhost',
-  address: 'smtp.mandrillapp.com',
-  port: '587',
-  enable_starttls_auto: true,
-  authentication: :plain,
-  user_name: ENV['MANDRILL_USER'],
-  password: ENV['MANDRILL_APIKEY']
-  }
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+
+  config.action_mailer.delivery_method = :file
+
+  # config.action_mailer.smtp_settings = {
+  # domain: 'localhost',
+  # address: 'smtp.mandrillapp.com',
+  # port: '587',
+  # enable_starttls_auto: true,
+  # authentication: :plain,
+  # user_name: ENV['MANDRILL_USER'],
+  # password: ENV['MANDRILL_APIKEY']
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
