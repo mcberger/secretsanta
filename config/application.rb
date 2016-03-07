221bdb028117dfb4b6f9e3ade702fa7aad44d937
Joe Spampinato <jcspampinato@gmail.com>
2016-03-07 17:37:05 -0500

    precompile application rb

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module Secretsanta
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.serve_static_assets = true
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-  end
+end
 end
